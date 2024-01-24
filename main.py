d8707ece39d859900e2aaed0eb4c6b661176b169
ywerling <ywerling@hotmail.de>
2024-01-24 08:38:14 +0100

    Create files

---
+++ main.py
@@ -0,0 +1,8 @@
+from pdfquery import PDFQuery
+
+# AI text-to-speech has come so far. They can sound more lifelike than a real audiobook.
+# Using what you've learnt about HTTP requests, APIs and Python scripting, create a program that can convert PDF files to speech.
+# You right want to choose your own Text-To-Speech (TTS) API. But here are some you can consider:
+# http://www.ispeech.org/api/#introduction
+# https://cloud.google.com/text-to-speech/docs/basics
+# https://aws.amazon.com/polly/
